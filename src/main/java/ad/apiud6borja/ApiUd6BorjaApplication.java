2adfa3764fb1c34a330daba0504ea86affc35ae0
Borja Cisneros <Borja.Cisneros@iessanmamede.com>
2024-02-21 15:26:32 +0100

    Initial commit - POM configured

---
+++ src/main/java/ad/apiud6borja/ApiUd6BorjaApplication.java
@@ -0,0 +1,13 @@
+package ad.apiud6borja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6BorjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUd6BorjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ad/apiud6borja/ApiUd6BorjaApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.apiud6borja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUd6BorjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
